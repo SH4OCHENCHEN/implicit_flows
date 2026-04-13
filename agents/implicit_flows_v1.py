f059a066e3dd4a1a6d4ac16fb165a17614ba58d5
SHAOCHENCHEN <353259950@qq.com>
2026-04-13 19:10:41 +0800

    up

--- agents/implicit_flows_v1.py
+++ agents/implicit_flows_v1.py
@@ -59,10 +59,12 @@ class ImplicitFlowsV1Agent(flax.struct.PyTreeNode):
             next_returns = (next_returns1 + next_returns2) / 2
 
         # The following returns will be bounded automatically
+        cur_noises = jax.random.normal(noise_rng, (batch_size, 1))
         returns = (jnp.expand_dims(batch['rewards'], axis=-1) +
                    self.config['discount'] * jnp.expand_dims(batch['masks'], axis=-1) * next_returns)
-        noisy_returns = times * returns + (1 - times) * noises
-        target_vector_field = returns - noises
+        ret_noises = jax.sqrt((1-self.config['discount'] ** 2)) * cur_noises + self.config['discount'] * noises
+        noisy_returns = times * returns + (1 - times) * ret_noises
+        target_vector_field = returns - ret_noises
 
         vector_field1 = self.network.select('critic_flow1')(
             noisy_returns, times, batch['observations'], batch['actions'], params=grad_params)
@@ -527,7 +529,7 @@ def get_config():
             clip_flow_actions=True,  # Whether to clip the intermediate flow actions.
             clip_flow_returns=True,  # Whether to clip flow returns.
             confidence_weight_temp=0.3,  # Temperature for the confidence weights.
-            dcfm_lambda=1.0,  # Distributional conditional flow matching loss coefficient.
+            dcfm_lambda=0.0,  # Distributional conditional flow matching loss coefficient.
             bcfm_lambda=1.0,  # Bootstrapped conditional flow matching loss coefficient.
             alpha=10.0,  # Flow distillation coefficient.
             normalize_q_loss=False,  # Whether to normalize the Q loss.
