ad1abfd093ebbc7128095f73e2f95afa22ad585e
SHAOCHENCHEN <353259950@qq.com>
2026-04-19 17:22:54 +0800

    up

--- agents/implicit_flows_v4.py
+++ agents/implicit_flows_v4.py
@@ -531,7 +531,7 @@ def get_config():
             next_return_clip_sigma=2.0,  # Sigma multiplier for Gaussian clipping anchor.
             next_return_clip_slack=0.05,  # Relaxation margin for lower/upper clipping bounds.
             bcfm_lambda=1.0,  # Bootstrapped conditional flow matching loss coefficient.
-            alpha=0.3,
+            alpha=2.0,
             encoder=ml_collections.config_dict.placeholder(str),
         )
     )
